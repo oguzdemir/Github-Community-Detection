46f62a9ce6a2fe79adbef114e6db3d098aed7492
Oguz Demir <oguz.demir@ug.bilkent.edu.tr>
2016-12-16 17:35:27 +0300

    Sample com. det. with colors

--- mainscript.py
+++ mainscript.py
@@ -1,6 +1,6 @@
 import igraph
-import cairocffi as cairo
 import csv
+
 
 
 #login,followers,following,languages,organizations
@@ -25,10 +25,11 @@ def addToMap (map, key1, key2, value):
 
 vertexMap = {}
 g = igraph.Graph()
+
 edgeMap = {}
 with open('users.csv','rb') as csvfile:
     reader = csv.reader(csvfile)
-    v_index = 0
+
     for row in reader:
         processingPerson = row[0].strip()
 
@@ -37,12 +38,9 @@ with open('users.csv','rb') as csvfile:
         for lang in languages:
             languageSet.append(lang)
 
-        v = Vertex(processingPerson, languageSet)
-        g.add_vertex(v)
-        g.vs["name"][v_index] = processingPerson
+        print "Adding" + processingPerson
 
-        vertexMap[processingPerson] = v_index
-        v_index += 1
+        g.add_vertex(name = processingPerson)
 
         followers = row[2][1:-1].split(",")
         for person in followers:
@@ -55,15 +53,21 @@ with open('users.csv','rb') as csvfile:
 
 count = 0
 for key,value in edgeMap.iteritems():
-    print count
     count += 1
     vertices = key.split("|")
-    v1 = vertexMap.get(vertices[0])
-    v2 = vertexMap.get(vertices[1])
-    g.add_edge(v1,v2,weight = value)
+    try:
+        g.add_edge(vertices[0],vertices[1],weight = value)
+        print "V: " + vertices[0] + " = " + vertices[1]
+    except ValueError:
+        print "Vertex not found: " + vertices[0] + " = " + vertices[1]
 
-a = g.community_leading_eigenvector(weights='weight')
+color_list = ['red', 'blue', 'green', 'cyan', 'pink', 'orange', 'grey', 'yellow', 'white', 'black', 'purple', 'magenta']
 
 
+community = g.community_multilevel()
+
+
+igraph.plot(g, bbox=(0,0,2000,2000), margin = (5,5,5,5), vertex_color=[color_list[x] for x in community.membership])
+print max(community.membership) #Printed number of communties to 11
 print 'x';
 
